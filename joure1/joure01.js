691376a8ba8cbcdf3e0aec7808c24abe34cf1132
Abdou-sow <you@example.com>
2021-03-09 14:32:10 +0100

    exercices joure01

---
+++ joure1/joure01.js
@@ -0,0 +1,44 @@
+//hello Word
+
+console.log("hello Word")
+
+//02 etc...
+
+console.log("etc...")
+
+// concatenation
+var test ="my na is Abdou"
+console.log(test)
+
+var name = "sow"
+console.log("nice to meet you" + " " + name)
+
+//string Length
+var testLength ="I'm vert lond !"
+console.log(testLength.length)
+
+//replace
+
+var food ="croissant is meh"
+food = food.replace("meh", "so good")
+console.log(food)
+
+//Up and Down
+
+var basic ="this is cool"
+var basicUp = basic.toUpperCase()//"THIS IS COOL"
+var basicDown = basic.toLocaleLowerCase()//"this is cool"
+
+console.log(basic, basicUp,basicDown)
+
+//Split
+var word="banana"
+var letters= word.split("")
+console.log(letters)
+
+//template
+
+var age = "23";
+var template = `I'm ${age} years old`;
+console.log(template);
+
