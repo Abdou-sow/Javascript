520cb33a3409e08a80a3734296f3ba824c3fe632
Abdou-sow <you@example.com>
2021-03-11 17:30:40 +0100

    add-Up

---
+++ joure04/Add-up.js
@@ -0,0 +1,10 @@
+function addUp(num){
+    if(num>1){
+        var totale=0
+        for(var i=0; i<=num;i++ ){
+                totale=totale+i
+        }
+    console.log(totale)
+    }
+}
+addUp(12)
