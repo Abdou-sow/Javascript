905fecceca06e9c850ac1ead73af1e83d4be8b03
Abdou-sow <you@example.com>
2021-03-17 17:34:16 +0100

    exo3

--- jour05/exo2/table.js
+++ jour05/exo2/table.js
@@ -1,16 +1,29 @@
-function multiply(nmb){
-    if(typeof(num)== "number"  && process.argv.length == 3 ){
-        var a =parseInt(num);
-        var rslt=0
-        for(var i=0; i<=10; i++){
-            rslt=i*a
-            console.log(num,"*",i,"=",rslt)
-        }
-    }
-    else{
-        console.log("error")
-    }
+// function multiply(nmb){
+//     if(typeof(num)== "number"  && process.argv.length == 3 ){
+//         var a =parseInt(num);
+//         var rslt=0
+//         for(var i=0; i<=10; i++){
+//             rslt=i*a
+//             console.log(num,"*",i,"=",rslt)
+//         }
+//     }
+//     else{
+//         console.log("error")
+//     }
+// }
+
+// var num = parseInt(process.argv[2])
+// multiply(num)
+var {multiply} = require("../exo3/table-utils")
+var num =parseInt(process.argv[2])
+
+
+if (typeof (num) == "number" && process.argv.length == 3) {
+    
+    multiply(num)
+
 }
 
-var num = parseInt(process.argv[2])
-multiply(num)
+else{
+    console.log("error")
+}
