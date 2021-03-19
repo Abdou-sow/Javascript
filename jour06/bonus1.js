f8623f433cd43b3042a72e6b32c650e2f9e34305
Abdou-sow <you@example.com>
2021-03-19 17:08:37 +0100

    bonus 1 jour06

--- jour06/bonus1.js
+++ jour06/bonus1.js
@@ -1,3 +1,15 @@
 var moment = require("moment");
-var today= moment().format('YYYY');
-console.log(today)
+
+function calculateAge2(str) {
+
+    var today = parseInt(moment().format('YYYY'));
+    var anne = parseInt(str.slice(6, 10))
+    var age = today - anne
+
+    console.log("tu as", age, "ans")
+
+
+}
+
+
+calculateAge2("15/04/2010")
